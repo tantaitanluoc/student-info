a7e40647d815c6875ee5a0cdf50ec9b14bd15ae2
Khan <thewayof.khan2407@gmail.com>
2019-05-27 16:48:59 +0700

    updated

--- admin/index.php
+++ admin/index.php
@@ -1,17 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Đăng nhập quyền quản trị</title>
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+<link href ="loginform.css" rel ="stylesheet">
+<title>Đăng nhập quyền quản trị</title>
 </head>
 <body>
-<div id="login-wrapper">
+<div id="login-wrapper" class ="jumbotron" >
+	<div class='container'>
+	<span class="glyphicon glyphicon-list-alt"></span>
+	<h2>Admin</h2>
+	<div class ='box'>
 	<form id= 'login-form' method="post" action = '<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>'>
-		<label for="username">Tên đăng nhập</label>
-		<input type = "text" name = 'username' required><br>
-		<label for="password">Mật khẩu</label>
-		<input type="password" name="password" required><br>
-		<input type="submit" name="submit" value="Đăng nhập">
+		<input type = "text" name = 'username' required placeholder ="Tên đăng nhập">
+		<input type="password" name="password" required placeholder="Mật khẩu">
+		<input type="submit" class="button" name="submit" value="Đăng nhập" >
 	</form>
+</div>
+</div>
 </div>
 
 <?php
