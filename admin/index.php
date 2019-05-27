4922becaf398ab309c3fd0969fe58974dae95599
unknown <tantaitanluoc@gmail.com>
2019-05-27 15:50:59 +0700

    tantai add adminpage

--- admin/index.php
+++ admin/index.php
@@ -4,9 +4,15 @@
 	<title>Đăng nhập quyền quản trị</title>
 </head>
 <body>
-
-
-
+<div id="login-wrapper">
+	<form id= 'login-form' method="post" action = '<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>'>
+		<label for="username">Tên đăng nhập</label>
+		<input type = "text" name = 'username' required><br>
+		<label for="password">Mật khẩu</label>
+		<input type="password" name="password" required><br>
+		<input type="submit" name="submit" value="Đăng nhập">
+	</form>
+</div>
 
 <?php
 require '../lib/Classes/PHPExcel.php';
@@ -14,7 +20,7 @@ require_once '../lib/Classes/PHPExcel/IOFactory.php';
 require '../lib/database.php';
 
 if(isset($_POST['submit'])){
-	
+
 }
 
 ?>
